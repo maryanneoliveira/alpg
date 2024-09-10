a34d7114154fedbb71d9f9c39dc4647c5a46a598
Maryane Oliveira <126249279+maryanneoliveira@users.noreply.github.com>
2024-09-09 21:36:20 -0300

    Add files via upload

---
+++ anotacoes/jogo_adivinhacao.py
@@ -0,0 +1,16 @@
+#ESTRUTURAS CONDICIONAIS (IF, ELSE)
+
+numero_secreto = 8;
+
+chute =  int(input("digite seu chute: "));
+
+if(chute == numero_secreto): #se os numeros forem iguais
+    print("Você acertou") #mostre VOCE ACERTOU
+
+
+    #OBS, NÃO É OBRIGATORIO TER O ELSE, O IF FUNCIONA SOZINHO
+
+else:
+    print("voce errou")
+
+#O ELSE NÃO TEM CONDIÇÃO ENTRE PARENTESES
